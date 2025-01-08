d0db1f99b6b4fd60a0916ed4957dedada3e8cebf
RayUTwolf <www.lovelion@gmail.com>
2025-01-08 16:22:24 +0800

    feat: framework.

--- src/views/GameTemplate/FindFood.vue
+++ src/views/GameTemplate/FindFood.vue
@@ -5,6 +5,11 @@
       <v-layer>
         <v-rect :config="configBG"></v-rect>
       </v-layer>
+      <v-layer>
+        <v-rect :config="configFrame1"> </v-rect>
+        <v-rect :config="configFrame2"> </v-rect>
+        <v-rect :config="configFrame3"> </v-rect>
+      </v-layer>
     </v-stage>
   </div>
 </template>
@@ -35,19 +40,41 @@ export default {
         fill: "gray",
         stroke: "gray",
       },
+      configFrame1: {},
+      configFrame2: {},
+      configFrame3: {},
     };
   },
   mounted() {
     this.initializeScene();
+    this.drawFrame();
   },
 
   methods: {
     initializeScene() {
       this.gameWidth = this.$refs.container.clientWidth;
+      this.gameHeight = this.gameWidth / 2;
       this.configKonva.width = this.gameWidth;
-      this.configKonva.height = this.gameWidth / 2;
-      this.configBG.width = this.gameWidth;
-      this.configBG.height = this.gameWidth / 2;
+      this.configKonva.height = this.gameHeight;
+      this.configBG.width = this.gameHeight;
+      this.configBG.height = this.gameHeight;
+    },
+    drawFrame() {
+      this.configFrame1.x = this.gameWidth / 2;
+      this.configFrame2.x = this.gameWidth / 2;
+      this.configFrame3.x = this.gameWidth / 2;
+      this.configFrame1.y = 0;
+      this.configFrame2.y = (this.gameHeight / 7) * 3;
+      this.configFrame3.y = (this.gameHeight / 7) * 6;
+      this.configFrame1.width = this.gameWidth / 2;
+      this.configFrame2.width = this.gameWidth / 2;
+      this.configFrame3.width = this.gameWidth / 2;
+      this.configFrame1.height = (this.gameHeight / 7) * 3;
+      this.configFrame2.height = (this.gameHeight / 7) * 3;
+      this.configFrame3.height = this.gameHeight / 7;
+      this.configFrame1.fill = "brown";
+      this.configFrame2.fill = "darkblue";
+      this.configFrame3.fill = "darkgreen";
     },
     update() {},
   },
