371fb750a4231c626431cb78bbf04e3277f336c7
RayUTwolf <www.lovelion@gmail.com>
2025-01-08 17:13:31 +0800

    feat: calculate ring radius and ratio.

--- src/views/GameTemplate/FindFood.vue
+++ src/views/GameTemplate/FindFood.vue
@@ -43,10 +43,12 @@ export default {
         strokeWidth: 5,
         draggable: true,
       },
+      ringRadius: [],
     };
   },
   mounted() {
     this.initializeScene();
+    this.setRingRadius();
     this.drawRing();
   },
 
@@ -59,10 +61,26 @@ export default {
       this.configBG.width = this.gameWidth / 2;
       this.configBG.height = this.gameHeight;
     },
+    setRingRadius() {
+      this.ringRadius[this.findLargestRadius()] = this.gameWidth / 5;
+
+      let ratio =
+        this.GameData.Radius[1 - this.findLargestRadius()] /
+        this.GameData.Radius[this.findLargestRadius()];
+      this.ringRadius[1 - this.findLargestRadius()] =
+        this.ringRadius[this.findLargestRadius()] * ratio;
+    },
+    findLargestRadius() {
+      if (this.GameData.Radius[0] < this.GameData.Radius[1]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    },
     drawRing() {
       this.configRing.x = (this.gameWidth / 4) * 3;
-      this.configRing.y = this.gameHeight / 2;
-      this.configRing.radius = this.gameWidth / 5;
+      this.configRing.y = this.gameHeight / 2 - this.gameWidth / 40;
+      this.configRing.radius = this.ringRadius[0];
     },
   },
 };
